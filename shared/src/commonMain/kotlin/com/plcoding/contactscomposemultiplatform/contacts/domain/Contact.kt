b3dcdf3950ebd8f489309dbe62bc5a86b7ba5c93
Rodrigo Barros <brodrigo87@gmail.com>
2023-08-09 17:39:53 -0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/contacts/domain/Contact.kt
@@ -0,0 +1,12 @@
+package com.plcoding.contactscomposemultiplatform.contacts.domain
+
+data class Contact(
+
+    val id: Long?,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val phoneNumber: String,
+    val photoBytes: ByteArray?
+)
+
