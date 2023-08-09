a41286bb36aaf5b8dec262c6f057d85ad6bd1ccc
Rodrigo Barros <brodrigo87@gmail.com>
2023-08-09 18:31:58 -0300

    ContactPhoto

---
+++ shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/contacts/presentation/components/ContactPhoto.kt
@@ -0,0 +1,20 @@
+package com.plcoding.contactscomposemultiplatform.contacts.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.plcoding.contactscomposemultiplatform.contacts.domain.Contact
+
+@Composable
+fun ContactPhoto(
+    contact: Contact?,
+    modifier: Modifier = Modifier,
+    iconSize: Dp = 25.dp
+) {
+
+    if(contact?.photoBytes != null) {
+
+    }
+
+}
